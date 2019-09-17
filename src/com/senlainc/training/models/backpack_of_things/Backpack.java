291c86c492eb38a10b775cd49961b6713be25b2f
mikhail <mikhailov.miail@yandex.ru>
2019-09-17 23:58:07 +0300

    refactoring code

--- src/com/senlainc/training/models/backpack_of_things/Backpack.java
+++ src/com/senlainc/training/models/backpack_of_things/Backpack.java
@@ -8,7 +8,10 @@ import org.json.simple.parser.ParseException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Backpack {
     private static final String PATH = "C:\\Users\\Компьютер\\IdeaProjects\\senla\\src\\com\\senlainc\\training\\resources\\things.json";
@@ -47,16 +50,30 @@ public class Backpack {
     public void buildBackpack() {
         this.parseJSON();
         int capacity = this.capacity;
-        for (Thing thing : this.things) {
+        List<Thing> things = this.things;
+        for (int i = 0; i < things.size(); i++) {
             if (capacity > 0) {
-                if (capacity >= thing.getWeight()) {
-                    capacity -= thing.getWeight();
+                List<Thing> th = things.subList(i, things.size());
+                Collections.max(th, Comparator.comparing(s -> s.getCost()));
+                Thing thingMax = Collections.max(th, Comparator.comparing(s -> s.getCost()));
+                if (capacity >= things.get(i).getWeight()) {
+                    double maxCost = things.get(i).getCost();
                 } else {
-                    this.things.remove(thing);
+                    things.remove(i);
                 }
             } else {
                 break;
             }
         }
+        System.out.println(things);
     }
+
+    @Override
+    public String toString() {
+        return "\tIn the backpack with capacity: "
+                + capacity +
+                " fit " + things.size() +
+                " things";
+    }
+
 }
